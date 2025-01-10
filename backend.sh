b0375c125ec3a97786d8649bfb6ad649a2644eda
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-10 14:38:37 +0530

    backend shell

--- backend.sh
+++ backend.sh
@@ -69,3 +69,23 @@ cd /app
 rm -rf /app/* #remov the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home.ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+#load the data before running backend
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Insalling MySQL Client"
+
+mysql -h mysql.advik.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
